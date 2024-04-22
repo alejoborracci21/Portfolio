ff3c0ebb5ea168ea21c73dbcade8a37ecaa3442e
Alejo Borracci <borraccialejo@hotmail.es>
2024-04-22 00:37:29 -0300

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import Perfil from './components/Perfil/Perfil';
 import Proyectos from './components/Proyectos/Proyectos';
-import styles from './app.module.css'
+import styles from './app.module.css';
 import Tecnologias from './components/Tecnologias/Tecnologias';
+import Navbar from './components/Navegacion/Navegacion';
 
 
 function App() {
   return (
     <>
-    <div className={styles.container}>
-       <Perfil />
-      <Proyectos/>
-      <Tecnologias/>
-    </div>
-     
+      <div className={styles.container}>
+        <Navbar />
+        <Perfil />
+        <Proyectos />
+        <Tecnologias />
+      </div>
     </>
   );
 }
--- src/components/Navegacion/Navegacion.jsx
+++ src/components/Navegacion/Navegacion.jsx
@@ -1,31 +1,132 @@
-import React from "react";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 
+const pages = ['Proyectos', 'Tecnologias', 'Contacto'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-const Navegacion = () => {
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    return(
-        <>
-        <div>
-            <button>
-                Sobre mi
-            </button>
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-            <button>
-                Proyectos
-            </button>
+  return (
+    <AppBar position="static" sx={{backgroundColor: 'black'}}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'white',
+              textDecoration: 'none',
+            }}
+          >
+            My Portfolio
+          </Typography>
 
-            <button>
-                Tecnologias
-            </button>
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
 
-            <button>
-                Contactame
-            </button>
-        </div>
-        </>
-    )
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
-
-export default Navegacion
+export default Navbar;
